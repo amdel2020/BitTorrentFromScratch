bf5ac621a76f335d6c802b99517d5e7a6ff21d3d
Amit Sharma <amit@Amits-MacBook-Pro.local>
2023-08-06 19:51:53 +0530

    First Commit - Inital Project

---
+++ src/main/java/com/amit/bittorrentfromscratch/BitTorrentFromScratchApplication.java
@@ -0,0 +1,13 @@
+package com.amit.bittorrentfromscratch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitTorrentFromScratchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitTorrentFromScratchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amit/bittorrentfromscratch/BitTorrentFromScratchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amit.bittorrentfromscratch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitTorrentFromScratchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
